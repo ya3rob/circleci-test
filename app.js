7afe335c61aa017c5fbd55265594d173276cf3ed
buhamad <ytb2000@hotmail.com>
2021-05-03 20:47:03 +0300

    6th v

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const http = require('http');
 
+console.log("working  ?")
 http.createServer(function (req, res) {
   res.write('Foo MEME version 5!');
   // i don't know  what to  do next
   res.end();
+
+  console.log('working');
 }).listen(8010, '0.0.0.0');
