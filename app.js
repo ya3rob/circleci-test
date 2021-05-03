70bb70b26811388d294e28305b5fa4503ab4328f
buhamad <ytb2000@hotmail.com>
2021-05-03 20:42:18 +0300

    new branch ?

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const http = require('http');
 
 http.createServer(function (req, res) {
-  res.write('Foo MEME version 4!');
+  res.write('Foo MEME version 5!');
+  // i don't know  what to  do next
   res.end();
 }).listen(8010, '0.0.0.0');
